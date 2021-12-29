9ece9a0d7caa7c7851be73dd1ed6ac645f7d977c
shrektan <shrektan@126.com>
2021-12-29 17:12:53 +0800

    convert date

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -2,6 +2,29 @@ use extendr_api::prelude::*;
 use chrono::{NaiveDate};
 mod bond;
 
+fn robj2date(x: Robj) -> NaiveDate {
+    if !x.inherits("Date") || x.len() != 1 {
+        panic!("must be a scalar Date");
+    } else {
+        let days = x.as_real().expect("must not be NA") as i32;
+        NaiveDate::from_num_days_from_ce(days + 719163)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn to_date() {
+        test! {
+            single_threaded(|| {
+                let x: Robj = r!(18990.0).set_class(&["Date"]).unwrap();
+                assert_eq!(robj2date(x), NaiveDate::from_ymd(2021, 12, 29));
+            });
+        }
+    }
+}
+
 /// Calculate the Bond's YTM, Maclay Duration, Modified Duration
 /// @param value_date,mty_date the value and maturity date of the bond
 /// @param redem_value,cpn_rate,cpn_freq the redemption value, coupon rate and coupon frequency of the bond.
@@ -9,15 +32,16 @@ mod bond;
 /// @return a double vector with 3 elements: ytm, macd and modd
 /// @export
 #[extendr]
-fn bond_result(value_date: &str, mty_date: &str, redem_value: f64, cpn_rate: f64, cpn_freq: u32, ref_date: &str, clean_price: f64) -> Robj {
+fn bond_result(value_date: Robj, mty_date: Robj, redem_value: f64, cpn_rate: f64, cpn_freq: u32, ref_date: Robj, clean_price: f64) -> Robj {
     let bond = bond::FixedBond::new(
-        NaiveDate::parse_from_str(value_date, "%Y-%m-%d").unwrap(),
-        NaiveDate::parse_from_str(mty_date, "%Y-%m-%d").unwrap(),
+        robj2date(value_date),
+        // NaiveDate::parse_from_str(value_date, "%Y-%m-%d").unwrap(),
+        robj2date(mty_date),
         redem_value,
         cpn_rate,
         cpn_freq
     );
-    let ref_date = NaiveDate::parse_from_str(ref_date, "%Y-%m-%d").unwrap();
+    let ref_date = robj2date(ref_date);
     let out = bond.result(&ref_date, clean_price);
     r!([out.ytm, out.macd, out.modd])
 }
