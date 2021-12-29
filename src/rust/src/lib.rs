978b91c8b5867c948f057a3a384681f7344d885f
shrektan <shrektan@126.com>
2021-12-29 19:24:30 +0800

    doc ref_date, clean_price

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -29,6 +29,7 @@ mod test {
 /// @param value_date,mty_date the value and maturity date of the bond
 /// @param redem_value,cpn_rate,cpn_freq the redemption value, coupon rate and coupon frequency of the bond.
 ///   Note that the **frequency** can only be one of 1, 2, 4, 0 (pay at mature)
+/// @param ref_date,clean_price the reference date and the clean price that used to calculate the bond results
 /// @return a double vector with 3 elements: ytm, macd and modd
 /// @export
 #[extendr]
