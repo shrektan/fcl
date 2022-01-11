10e9d168dc85b5212acfebabe80d322f20111eb3
shrektan <shrektan@126.com>
2022-01-11 15:43:50 +0800

    fix cmd

--- R/rtn.R
+++ R/rtn.R
@@ -4,8 +4,9 @@
 #' calucating the time-weighted rate of rtn (twrr) or modified
 #' dietz rate of return (dietz).
 #'
-#' @param ids an integer vector, the ID each row belongs to
-#' @param dates a Date vector, the
+#' @param ids an integer vector, the ID of each row belongs to
+#' @param dates a Date vector, the reference date of each row
+#' @param mvs,pls a Double vector, the market value and the PnL of each day
 #' @details All the input vector must be the same length.
 #' @export
 create_rtn <- function(ids, dates, mvs, pls) {
@@ -23,32 +24,32 @@ create_rtn <- function(ids, dates, mvs, pls) {
   out <- new.env()
   out$.self <- obj
   out$twrr_cr <- function(id, from, to) {
+    .self <- out$.self
     xts::xts(
       .self$twrr_cr(as.integer(id), from, to),
       .self$dates(from, to)
     )
   }
   out$twrr_dr <- function(id, from, to) {
+    .self <- out$.self
     xts::xts(
       .self$twrr_dr(as.integer(id), from, to),
       .self$dates(from, to)
     )
   }
   out$dietz <- function(id, from, to) {
+    .self <- out$.self
     xts::xts(
       .self$dietz(as.integer(id), from, to),
       .self$dates(from, to)
     )
   }
   out$dietz_avc <- function(id, from, to) {
+    .self <- out$.self
     xts::xts(
       .self$dietz_avc(as.integer(id), from, to),
       .self$dates(from, to)
     )
   }
-  vars <- ls(out)
-  invisible(lapply(vars, function(var) {
-    environment(out[[var]]) <- out
-  }))
   out
 }
