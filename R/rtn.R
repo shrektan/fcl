6241f8025c4dbf33d5c7cc20164ac1e17ddf3080
shrektan <shrektan@126.com>
2022-01-19 04:25:49 +0800

    add doc for make_rtn

--- R/rtn.R
+++ R/rtn.R
@@ -8,9 +8,37 @@
 #' dietz rate of return (dietz).
 #'
 #' @param date a Date vector, the reference date of each row
-#' @param mv,pl a Double vector, the market value and the PnL of each day
+#' @param mv,pl a double vector, the market value and the PnL of each day
 #' @param id an integer vector, the ID of each row belongs to
-#' @details All the input vector must be 1 or the same length.
+#' @section Cash flow handling:
+#'   * The cash flow is not provided externally. Instead, it's deducted via
+#'     market value and PnL, with the equation \eqn{\Delta MV = \Delta PnL + CF}.
+#'   * The cash inflow is treating as if it happens at the beginning of the day,
+#'     while the cash outflow is at the end of the day. The reasons are two.
+#'     The first is to reduce the possibility of having a close-to-zero denominator.
+#'     The second is the cash is usually not usable for the whole day.
+#'   * The calculation is based on calendar days. No business calendar or weekday
+#'     considers. You can't change the calculation frequency, either. However,
+#'     this is possible in the future version.
+#' @references
+#' Modified Dietz Method: https://en.wikipedia.org/wiki/Modified_Dietz_method
+#'
+#' Time weighed Return: https://en.wikipedia.org/wiki/Time-weighted_return
+#' @note All the input vector must be 1 or the same length.
+#' @return A list of functions, with signature of `from`, `to` and `id`, all of
+#'   which are only allowed to accept a scalar. They all return an `xts` object
+#'   with one column.
+#'   * `twrr_cr`: the cumulative time weighted return
+#'   * `twrr_dr`: the daily time weighted return
+#'   * `dietz`: the Modified Dietz Return
+#'   * `dietz_avc`: the denominator used to calculate the Modified Dietz Return
+#'   * `cum_pl`: the cumulative PnL
+#' @examples
+#' rtn <- make_rtn(date = c(210101, 210105, 210110), mv = c(100, 123, 140), pl = c(0, 3, 7))
+#' rtn$twrr_cr(210102, 210110)
+#' rtn$twrr_dr(210102, 210110)
+#' rtn$dietz(210102, 210110)
+#' rtn$dietz_avc(210102, 210110)
 #' @export
 make_rtn <- function(date, mv, pl, id = 1L) {
   args <- prepare_args(
